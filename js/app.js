951b7aa35d8b253a86fd823ab69c6c5bcafe91ba
Flavio Santos <flaviohenriqu@gmail.com>
2017-01-02 14:29:48 -0300

    Manage Dropdowns page

--- js/app.js
+++ js/app.js
@@ -1,16 +1,29 @@
 var recruiterApp = angular.module('recruiterApp', ['ui.router']);
 
+recruiterApp.controller('DropdownController', ['$scope', '$http', function($scope, $http){
+    $scope.dropdowns;
+    $http.get('http://127.0.0.1:8000/api/v1/dropdowns/?format=json').then(function(response) {
+      $scope.dropdowns = response.data;
+  });
+}]);
+
+
 recruiterApp.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/home');
 
-    $stateProvider
-      .state('home', {
+    var homeState = {
+        name: 'home',
         url: '/home',
         templateUrl: 'partial-home.html'
-      })
-      .state('dropdowns', {
-        // add dropdown page
-      })
-      .state('candidates', {})
-      .state('tags', {});
+    }
+    var dropState = {
+        name: 'dropdowns',
+        url: '/dropdowns',
+        templateUrl: 'partial-dropdown.html',
+        controller: 'DropdownController'
+    }
+
+    $stateProvider.state(homeState);
+    $stateProvider.state(dropState);
 });
+
