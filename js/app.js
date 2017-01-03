eda73d504389b326491df719ed74aba9f614b6d6
Flavio Santos <flaviohenriqu@gmail.com>
2017-01-03 19:07:24 -0300

    Adding cache, function valuesForField

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var recruiterApp = angular.module('recruiterApp', ['ui.router']);
 
-recruiterApp.factory('DropdownService', ['$http', function($http){
+recruiterApp.factory('DropdownService', ['$http','$q', '$filter', '$cacheFactory', function($http, $q, $filter, $cacheFactory){
+  var values;
   var service = {
     allFields: allFields,
     valuesForField: valuesForField,
@@ -8,12 +9,15 @@ recruiterApp.factory('DropdownService', ['$http', function($http){
   }
   return service;
 
-  function allFields() {
-      return $http.get('http://localhost:8000/api/v1/dropdowns/?format=json');
+  function valuesForField(fieldName) {
+    var result = $filter('filter')(values, {field_name: fieldName});
+    console.log(result);
+    return result;
   }
 
-  function valuesForField(fieldName) {
-    //
+  function allFields() {
+    var dropCache = $cacheFactory.get('dropCache');
+    return($http.get('http://localhost:8000/api/v1/dropdowns/?format=json', {cache: dropCache}).then(handleSuccess, handleError));
   }
 
   function addValue(fieldName, value) {
@@ -21,25 +25,53 @@ recruiterApp.factory('DropdownService', ['$http', function($http){
             field_name: fieldName,
             dropdown_values: [{text: value}],
     }
-    return $http.post('http://localhost:8000/api/v1/dropdowns/', dataObj)
+    return($http.post('http://localhost:8000/api/v1/dropdowns/', dataObj).then(handleSuccess, handleError))
+  }
+
+  function handleError(response){
+    if(!angular.isObject(response.data) || !response.data.message){
+      return($q.reject("An unknown error occurred."));
+    }
+    return($q.reject(response.data.message));
+  }
+
+  function handleSuccess(response){
+    return(response.data);
   }
 }]);
 
 recruiterApp.controller('DropdownController', ['$scope', 'DropdownService', function($scope, DropdownService){
-    var self = this;
     $scope.dropdowns;
 
-    self.allFields = function(){
-      DropdownService.allFields().then(function(response){
-        $scope.dropdowns = response.data;
-      });
-    }
-    self.allFields();
+    allFields();
 
     $scope.submit = function(fieldName, id) {
       console.log('Saving new dropdown value.');
       var value = angular.element('#field_'+id).val();
-      DropdownService.addValue(fieldName, value).then(self.allFields());
+      DropdownService.addValue(fieldName, value)
+        .then(
+            allFields(),
+            function(errorMessage){
+              console.warn(errorMessage);
+            }
+            );
+    }
+
+    $scope.valuesForField = function(fieldName) {
+      DropdownService.valuesForField(fieldName);
+    }
+
+    function applyRemoteData(dropdowns){
+      $scope.dropdowns = dropdowns;
+    }
+
+    function allFields(){
+      DropdownService.allFields()
+        .then(
+              function(values){
+                applyRemoteData(values);
+              }
+            );
     }
 }]);
 
